2365c97119c9b096053da9f27c1583fb39f3bbae
44 <44x1carbon@gmail.com>
2022-02-18 01:48:41 +0900

    レポート出力機能実装

---
+++ gen-report/index.js
@@ -0,0 +1,36 @@
+const fs = require("fs");
+const path = require("path");
+const TextLintEngine = require("textlint").TextLintEngine;
+
+const engine = new TextLintEngine({
+  configFile: ".textlintrc",  
+});
+const pug = require('pug');
+
+engine.executeOnFiles(["../curriculum/**/*.md"]).then((results) => {  
+  if (engine.isErrorResults(results)) {
+    results = results.map(result => {
+      return {
+        fileName:  path.basename(result.filePath),
+        ...result
+      }
+    });
+    const files = results.map(result => {
+      return {
+        fileName: path.basename(result.filePath),
+        lines: fs.readFileSync(result.filePath).toString().split("\n"),
+      }
+    }).reduce((p,c) => {
+      p[c.fileName] = c;
+      return p;
+    }, {});
+
+    fs.writeFileSync("../textlint-report.html", pug.renderFile(path.resolve(__dirname, 'template.pug'), {
+      results,
+      files
+    }));
+    console.log("Generate Report Success!!");
+  } else {
+    console.log("All Passed!");
+  }
+});
