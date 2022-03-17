4497d1c95cc999807427a0da3dd1fcdca6258e68
44 <44x1carbon@gmail.com>
2022-03-17 09:56:09 +0900

    ソースコードの行数を警告するルール作成

--- textlint-rule-preset-techpit-host-guide/src/index.js
+++ textlint-rule-preset-techpit-host-guide/src/index.js
@@ -1,8 +1,12 @@
 module.exports = {
   rules: {
     "codeblock-modify-comment": require("./codeblock-modify-comment"),
+    "codeblock-linenum-limit": require("./codeblock-linenum-limit")
   },
   rulesConfig: {
     "codeblock-modify-comment": true,
+    "codeblock-linenum-limit": {
+      limitLineNum: 10
+    }
   },
 };
