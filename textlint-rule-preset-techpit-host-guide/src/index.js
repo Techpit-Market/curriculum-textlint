91eade117f03647fa96f462310054d732921e165
Takuma Tachibana <moja0316.takuma@gmail.com>
2022-03-24 01:05:00 +0900

    h4以降を警告するルールの作成

--- textlint-rule-preset-techpit-host-guide/src/index.js
+++ textlint-rule-preset-techpit-host-guide/src/index.js
@@ -1,6 +1,7 @@
 module.exports = {
   rules: {
     "codeblock-modify-comment": require("./codeblock-modify-comment"),
+    "do-not-use-h4": require("./do-not-use-h4"),
   },
   rulesConfig: {
     "codeblock-modify-comment": true,
