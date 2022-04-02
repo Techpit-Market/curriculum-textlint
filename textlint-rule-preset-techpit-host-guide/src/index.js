7b865774d6bcd8b9a88abd9b39577edc481da53e
Takuma Tachibana <moja0316.takuma@gmail.com>
2022-04-02 14:19:53 +0900

    1パート10000字を大きく超える場合警告

--- textlint-rule-preset-techpit-host-guide/src/index.js
+++ textlint-rule-preset-techpit-host-guide/src/index.js
@@ -1,8 +1,10 @@
 module.exports = {
   rules: {
     "codeblock-modify-comment": require("./codeblock-modify-comment"),
+    "over-1000words": require("./over-10000words"),
   },
   rulesConfig: {
     "codeblock-modify-comment": true,
+    "over-1000words": true,
   },
 };
