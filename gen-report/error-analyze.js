81830094ab8ae1c9897bc589f7dbb7b906fb62fa
44 <44x1carbon@gmail.com>
2022-08-09 10:23:19 +0900

    エラー集計機能実装

---
+++ gen-report/error-analyze.js
@@ -0,0 +1,31 @@
+const fs = require("fs");
+const path = require("path");
+const TextLintEngine = require("textlint").TextLintEngine;
+
+const engine = new TextLintEngine({
+  configFile: "./curriculum-textlint/.textlintrc",
+});
+
+const chapter = process.argv[2];
+
+(async () => {
+  const results = await engine.executeOnFiles([
+    `./curriculum/${chapter}*/*.md`,
+  ]);
+
+  const report = {};
+
+  results.forEach(({ messages, filePath }) => {
+    console.log(filePath);
+    messages.forEach((message) => {
+      console.log(message);
+      if (report[message.ruleId] === undefined) {
+        report[message.ruleId] = 0;
+      }
+
+      report[message.ruleId]++;
+    });
+  });
+
+  console.log(report);
+})();
